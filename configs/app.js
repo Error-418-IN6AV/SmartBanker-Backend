fb1a5d0f5e4d68023a93841162356d849c626c48
avillanueva-2021436 <avillanueva-2021436@kinal.edu.gt>
2023-06-02 13:26:14 -0600

    Se agrego ruta de user

--- configs/app.js
+++ configs/app.js
@@ -6,12 +6,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3500;
+const userRoutes = require('../src/user/user.routes');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+app.use('/user',userRoutes)
+
 
 exports.initServer = ()=>{
     app.listen(port);
