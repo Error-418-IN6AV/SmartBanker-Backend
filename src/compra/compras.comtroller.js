c308db680ea13825ed61359e116ce4fd5dcebffb
carroyo-2021203 <carroyo-2021203@kinal.edu.gt>
2023-06-30 18:42:45 -0600

    Se actulizo crud de compras y fatura

--- src/compra/compras.comtroller.js
+++ src/compra/compras.comtroller.js
@@ -8,7 +8,7 @@ exports.add = async (req, res) => {
     try {
         let data = req.body;
         let user = req.user.sub
-
+//////// parte de comprar solo un elemento 
 /*         let existCompra = await Compra.findOne({ user: user, product: data.product }); */
 
         let existProducto = await Product.findOne({ _id: data.product })
